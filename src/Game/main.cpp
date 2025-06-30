c63dc1143ef6778b430a4df0cfdeb8bf301b8856
james wolfley <james.wolfley.jr@gmail.com>
2025-06-29 23:18:46 -0400

    changed sleep behavior to sleep in a spin lock

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -1,71 +1,46 @@
 #include "Engine/Core/Window.hpp"
 #include "Engine/Debug/DebugManager.hpp"
-#include "Engine/Rendering/RenderManager.hpp"
+#include <chrono>
 #include <iomanip>
 #include <iostream>
-#include <ostream>
 #include <stdlib.h>
 #include <thread>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
-constexpr double TARGET_FPS = 240.0;
+constexpr double TARGET_FPS = 120.0;
 constexpr double TARGET_FRAME_DT = 1.0 / TARGET_FPS;
 constexpr double TICK_RATE = 20.0;
 constexpr double TICK_DT = 1.0 / TICK_RATE;
 
 int main() {
     Engine::Window window(800, 600, "Factory Game");
-    Engine::RenderManager *render = new Engine::RenderManager();
-    double last_time = glfwGetTime();
-    double tick_lag = 0;
-    double current_delta_time = 0;
-    double frame_start = 0;
-    double last_tick_time = 0;
-    auto &debug = Engine::Debug::DebugManager::getInstance();
-    while (!window.shouldClose()) {
-        // timing
-        frame_start = glfwGetTime();
 
-        // Poll events than handle them.
+    double tick_lag = 0;
+    double previous_frame_start = 0;
+    auto &debug = Engine::Debug::DebugManager::getInstance();
+
+    while (!window.shouldClose()) {
+        double frame_start = glfwGetTime();
+        double current_frame_delta = frame_start - previous_frame_start;
+        previous_frame_start = frame_start;
         window.pollEvents();
-        double current_time = glfwGetTime();
-        current_delta_time = current_time - last_time;
-        last_time = current_time;
-        tick_lag += current_delta_time;
-        // Do a fixed "Physics update"
+
+        tick_lag += current_frame_delta;
         while (tick_lag >= TICK_DT) {
             tick_lag -= TICK_DT;
-            double now = glfwGetTime();
-            double tick_delta = now - last_tick_time;
-            last_tick_time = now;
-            DEBUG_UPS(tick_delta);
         }
 
-        current_delta_time = glfwGetTime() - frame_start;
-        // Render here
-        render->render(&window, current_delta_time);
-
-        // Do frame update here
-
-        current_delta_time = glfwGetTime() - frame_start;
-        // Sleep rest of frame time till its time to render a new frame.
-        if (current_delta_time < TARGET_FRAME_DT) {
-            double sleep_duration = TARGET_FRAME_DT - current_delta_time;
-            std::this_thread::sleep_for(
-                std::chrono::duration<double>(sleep_duration - 0.001));
-        }
         do {
-            current_delta_time = glfwGetTime() - frame_start;
-        } while (current_delta_time <= TARGET_FRAME_DT);
-        DEBUG_FPS(current_delta_time);
-        // Swap the buffers to update the screen
-        system("clear");
-        std::cout << std::setprecision(4) << std::fixed << debug.getFPS()
-                  << std::endl
-                  << debug.getUPS() << std::endl;
+            std::this_thread::sleep_for(
+                std::chrono::duration<double>(0.0000000001));
+            // std::this_thread::yield();
+            current_frame_delta = glfwGetTime() - frame_start;
+        } while (current_frame_delta <= TARGET_FRAME_DT);
+        DEBUG_FPS(current_frame_delta);
+        std::cout << std::fixed << std::setprecision(10) << debug.getFPS()
+                  << std::endl;
         window.swapBuffers();
     }
-    delete render;
     return 0;
 }
